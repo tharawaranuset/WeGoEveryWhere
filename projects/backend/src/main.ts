1e2caf9ea5fc9e0e7f27df5b1e4ab78a913c8ca8
Thara Waranuset <tharawaranuset@gmail.com>
2025-08-17 13:43:41 +0700

    Feat: add swagger

--- projects/backend/src/main.ts
+++ projects/backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from '@core/swagger/setupSwagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  setupSwagger(app, configService);
+
+  const port = configService.get<number>('app.port', 3001);
+  await app.listen(port);
 }
 bootstrap();

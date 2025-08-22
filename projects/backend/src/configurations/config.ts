f1430313246968eaf787bba0621cc2a14648f862
Thara Waranuset <tharawaranuset@gmail.com>
2025-08-22 11:22:13 +0700

    Update: config

---
+++ projects/backend/src/configurations/config.ts
@@ -0,0 +1,9 @@
+import { ConfigModule } from "@nestjs/config";
+import { ENV_PATHS } from "@consts/env_paths";
+import { authConfig, swaggerConfig, appConfig, postgresConfig } from "./configs/index";
+
+export const AppConfigModule = ConfigModule.forRoot({
+  isGlobal: true,
+  envFilePath: [ENV_PATHS.ROOT, ENV_PATHS.BACKEND],
+  load: [appConfig, postgresConfig, swaggerConfig, authConfig],
+});
---
+++ projects/backend/src/configurations/configs/index.ts
@@ -0,0 +1,4 @@
+export {default as authConfig} from './auth.config';
+export {default as appConfig} from './app.config';
+export {default as postgresConfig} from './postgres.config';
+export {default as swaggerConfig} from './swagger.config';

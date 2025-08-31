d0dde3d0d2e3a6b78c703cb094a0f2763fa45b64
Jojoemc2 <earthquake.thailand@gmail.com>
2025-09-01 01:57:20 +0700

    Init

--- projects/backend/src/app.service.ts
+++ projects/backend/src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  CreateEvent(createEventDto): string{
+    return 'Create Event!';
+  };
+
 }
--- projects/backend/src/main.ts
+++ projects/backend/src/main.ts
@@ -6,3 +6,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+console.log("WegoEverywhere is Running");

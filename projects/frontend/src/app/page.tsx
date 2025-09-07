15548681219798c96e16f6feb160bb887829b4ce
kaiartty <pangpangpit.art@gmail.com>
2025-09-07 16:18:35 +0700

    Add fonts and fix Navbar

--- projects/frontend/src/app/page.tsx
+++ projects/frontend/src/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-
-export default async function Home() {
-  await new Promise((resolve) => setInterval(resolve, 1000));
+export default function Home() {
   return (
     <>
-      <div style={{ justifyContent: "center", display: "flex" }}>Home Page</div>
+      <p className="font-sans font-black">นี่คือ Karla (ฟอนต์หลัก)</p>
+      <p className="font-alt font-thin">นี่คือ Urbanist (ฟอนต์รอง)</p>
     </>
   );
 }

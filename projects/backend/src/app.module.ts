f1a3200d442f29296f0970140b72bf849c13b544
unknown <6631342521@student.chula.ac.th>
2025-09-11 01:48:06 +0700

    Add event to appmodule

--- projects/backend/src/app.module.ts
+++ projects/backend/src/app.module.ts
@@ -6,11 +6,13 @@ import { AuthModule } from '@core/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard } from './core/auth/jwt/access-jwt/jwt.guard';
 import { DatabaseModule} from './database/database.module';
+import { EventsModule } from './core/event/event.module'; // <-- 1. Import EventsModule
 
 @Module({
   imports: [
     AppConfigModule,
     AuthModule,
+    EventsModule,
     DatabaseModule
   ],
   controllers: [AppController],

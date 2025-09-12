efca49b028c3a58574c88732873558c2d2dd04b2
unknown <6631342521@student.chula.ac.th>
2025-09-12 20:10:41 +0700

    Update_user and All event api

---
+++ projects/backend/src/modules/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common'; // <-- Add NotFoundException
+import { NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres';
+import { eq } from 'drizzle-orm';
+import { Pool } from 'pg';     
+import { schema } from '@backend/src/database/schema';
+import { UpdateUserDto } from './users.dto'; // <-- Import the DTO
+
+@Injectable()
+export class UserService{
+    private readonly db: NodePgDatabase<typeof schema>;
+
+  constructor(
+    private readonly pool: Pool,
+  ) {
+    this.db = drizzle(this.pool, { schema });
+  }
+
+  async findAll(){
+    return this.db.query.users.findMany();
+  }
+
+  async update(id : number , updateuserdto : UpdateUserDto){
+    const[updateuser] = await this.db
+    .update(schema.users)
+    .set(updateuserdto)
+    .where(eq(schema.users.userId, id))
+    .returning();
+  if(!updateuser){
+    throw new NotFoundException(`User with ID ${id} not found.`);
+  }
+  return updateuser;
+  }
+}

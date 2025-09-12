dda62514cfd87b26de2621b8d767cbdb3ee8dee2
whatiskk <kwankaothanyaporn2015@gmail.com>
2025-09-12 21:14:10 +0700

    fix : password input size

--- projects/frontend/src/components/form/input/PasswordInput.tsx
+++ projects/frontend/src/components/form/input/PasswordInput.tsx
@@ -39,9 +39,9 @@ export default function PasswordInput({
           required
           placeholder={placeholder}
           className="
-            w-full rounded-3xl bg-white border border-black
-            px-4 pr-11 py-2.5 text-base
-            focus:outline-none focus:ring-2 focus:ring-brand-orange
+            w-full rounded-3xl border border-black bg-white
+            px-4 py-2.5 text-base
+            focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-orangee
           "
         />
 
